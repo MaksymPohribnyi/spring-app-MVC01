164429000f07f89f814e30cff801c36f4b1facb4
hp pavilion <hp pavilion@10.0.90.39>
2023-10-20 18:12:25 +0300

    enable spring config files and db properties

---
+++ SpringMVC.CRUD-APP01/src/main/java/ua/pohribnyi/weblibrary/config/DispatcherServletInitializer.java
@@ -0,0 +1,38 @@
+package ua.pohribnyi.weblibrary.config;
+
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { SpringAppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
+		registerHiddenFieldFilter(servletContext);
+	}
+
+	private void registerHiddenFieldFilter(ServletContext aContext) {
+		aContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true,
+				"/*");
+	}
+
+}
